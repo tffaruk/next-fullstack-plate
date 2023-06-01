d2e8df5fc8f6a22d9d8894c19cefb8a2bba21996
Omar Faruk <faruk.themefisher@gmail.com>
2023-06-01 13:31:40 +0600

    fixing auth erro

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
 import NextAuth from "next-auth";
-import CredentialsProvide from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 export const authoptions = {
   secret: process.env.NEXT_AUTH_SECRET,
@@ -8,34 +6,32 @@ export const authoptions = {
     strategy: "jwt" as any,
   },
   providers: [
-    CredentialsProvide({
-      type: "credentials",
+    // CredentialsProvide({
+    //   type: "credentials",
 
-      credentials: {},
-      async authorize(credentials: any, req: any): Promise<any> {
-        // Add logic here to look up the user from the credentials supplied
-        const { email, password } = credentials;
+    //   credentials: {},
+    //   async authorize(credentials: any, req: any): Promise<any> {
+    //     // Add logic here to look up the user from the credentials supplied
+    //     const { email, password } = credentials;
 
-        const user = await axios.get("http://localhost:3000/api/getuser");
-        console.log();
+    //     const user = await axios.get("http://localhost:3000/api/getuser");
+    //     console.log();
 
-        // const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
+    //     if (
+    //       user.data.data.user.map((data: any) => data.email).includes(email) &&
+    //       user.data.data.user
+    //         .map((data: any) => data.password)
+    //         .includes(password)
+    //     ) {
+    //       return {
+    //         email: email,
 
-        if (
-          user.data.data.user.map((data: any) => data.email).includes(email) &&
-          user.data.data.user
-            .map((data: any) => data.password)
-            .includes(password)
-        ) {
-          return {
-            email: email,
-
-            // role: res.data.result[0].role,
-          };
-        }
-        return null;
-      },
-    }),
+    //         // role: res.data.result[0].role,
+    //       };
+    //     }
+    //     return null;
+    //   },
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
@@ -56,47 +52,6 @@ export const authoptions = {
     },
   },
 };
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
-
-// export const authoptions = {
-//   secret: process.env.NEXT_AUTH_SECRET,
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_ID as string,
-//       clientSecret: process.env.GOOGLE_SECRET as string,
-//     }),
-//     CredentialsProvide({
-//       type: "credentials",
-
-//       credentials: {},
-//       async authorize(credentials: any, req: any): Promise<any> {
-//         // Add logic here to look up the user from the credentials supplied
-//         const { email, password } = credentials;
-//         const res = await axios.get("admin");
-
-//         // const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
-
-//         if (
-//           res.data.result.map((data: any) => data.email).includes(email) &&
-//           res.data.result.map((data: any) => data.password).includes(password)
-//         ) {
-//           return {
-//             email: email,
-
-//             // role: res.data.result[0].role,
-//           };
-//         }
-//         return null;
-//       },
-//     }),
-//   ],
-//   pages: {
-//     signIn: "/signin",
-//   },
-// };
-
 const handler = NextAuth(authoptions);
 
 export { handler as GET, handler as POST };
