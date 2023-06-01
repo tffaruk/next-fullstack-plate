45f375acd950de85d4e4aae80c62d4333d883bcd
Omar Faruk <faruk.themefisher@gmail.com>
2023-06-01 13:31:20 +0600

    fixing auth erro

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,13 +1,14 @@
+import LoginComponent from "@/components/LoginComponent";
 import { getProviders } from "next-auth/react";
 import { Suspense } from "react";
 
 const SignInPage = async () => {
   const providers = (await getProviders()) || {};
   console.log(providers);
-  // const authProviders = Object.values(providers as any) || {};
+  const authProviders = Object.values(providers as any) || {};
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      {/* <LoginComponent authProviders={authProviders} /> */}
+      <LoginComponent authProviders={authProviders} />
     </Suspense>
   );
 };
