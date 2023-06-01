6fe532633eab11ee72d1d45b6b5050158ac7a6e9
Omar Faruk <faruk.themefisher@gmail.com>
2023-06-01 12:43:26 +0600

    add auth plate

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,15 @@
 import LoginComponent from "@/components/LoginComponent";
 import { getProviders } from "next-auth/react";
+import { Suspense } from "react";
 
 const SignInPage = async () => {
   const providers = await getProviders();
 
-  return <LoginComponent providers={providers} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginComponent providers={providers} />
+    </Suspense>
+  );
 };
 
 export default SignInPage;
